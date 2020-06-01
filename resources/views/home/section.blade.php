628790dbf4f70af727c67021c3d4224f5c2023ce
icslbd <team.infocrat@gmail.com>
2020-06-01 13:00:13 +0600

    ok

--- resources/views/home/section.blade.php
+++ resources/views/home/section.blade.php
@@ -1,27 +1,28 @@
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 col-md-4" style="height: 20%">
-              <div class="card shadow p-3 mb-2 bg-white rounded">
-                <div class="card-body" style="height: 20%">
+            <div class="col-sm-12 col-md-4"  >
+              <div class="card shadow p-3 mb-2 bg-white rounded" style="height: 32vh;">
+                <div class="card-body">
                   <p style="text-align: center"><i class="fas fa-home" style="font-size:60px;color: blue"></i></p>
                   <h6 class="card-title" style="text-align: center">Home  Delivery</h6>
                   <p class="card-text" style="text-align: center">After print we deliver your product to your door step.</p>
+                  <br/>
                 </div>
               </div>
             </div>
-            <div class="col-sm-12 col-md-4" style="height: 20%">
-              <div class="card shadow p-3 mb-2 bg-white rounded" >
-                <div class="card-body" style="height: 20%">
+            <div class="col-sm-12 col-md-4"  >
+              <div class="card shadow p-3 mb-2 bg-white rounded" style="height: 32vh;">
+                <div class="card-body" >
                     <p style="text-align: center"><i class="far fa-gem" style="font-size:60px;color:blue"></i></p>
                   <h6 class="card-title" style="text-align: center">Re-Print Guarantee</h6>
                   <p class="card-text" style="text-align: center">If your printed goods are faulty. We will Re-print it with our cost.</p>
                 </div>
               </div>
             </div>
-            <div class="col-sm-12 col-md-4" style="height: 20%">
-                <div class="card shadow p-3 mb-2 bg-white rounded">
-                  <div class="card-body" style="height: 20%">
+            <div class="col-sm-12 col-md-4"  >
+                <div class="card shadow p-3 mb-2 bg-white rounded" style="height: 32vh;">
+                  <div class="card-body">
                     <p style="text-align: center"><i class="fas fa-user-clock" style="font-size:60px;color:blue"></i> </p>
                     <h6 class="card-title" style="text-align: center">Support 11 am - 7 pm</h6>
                     <p class="card-text"  style="text-align: center">We are here for your support 11 am - 7 pm Saturday- Thursday.</p>
